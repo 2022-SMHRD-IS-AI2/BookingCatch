dc4a4a32e6d0602259eba3191ee5978d6cad3d17
SMHRD <SMHRD@121.147.52.4>
2023-01-05 15:43:17 +0900

    230105 김민현 패키지생성 및 memberVO만들기시도

---
+++ BookingCatch/src/main/java/com/smhrd/model/MemberVO.java
@@ -0,0 +1,15 @@
+package com.smhrd.model;
+
+public class MemberVO {
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String birth;
+	private String mbti;
+	private String address;
+	private String date;
+	private String phone;
+	private String gender;
+
+}
