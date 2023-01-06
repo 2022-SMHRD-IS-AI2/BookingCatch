c5d11a55d3a8c0aa2b85a29340068a24cab0fc78
daheeisLegend <120437548+daheeisLegend@users.noreply.github.com>
2023-01-06 10:58:52 +0900

    김다희 생성자 추가

--- BookingCatch/src/main/java/com/smhrd/controller/JoinCon.java
+++ BookingCatch/src/main/java/com/smhrd/controller/JoinCon.java
@@ -14,8 +14,13 @@ public class JoinCon extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String id = request.getParameter("id");
-		
-		
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String birth = request.getParameter("birth");
+		String mbti = request.getParameter("mbti");
+		String address = request.getParameter("address");
+		String phone = request.getParameter("phone");
+		String gender = request.getParameter("gender");
 		
 		
 		
