07b51fa953fe3a13dbfa41b6f0e8c7c8ac358dcc
daheeisLegend <120437548+daheeisLegend@users.noreply.github.com>
2023-01-06 10:57:10 +0900

    230106 생성자추가

--- BookingCatch/src/main/java/com/smhrd/model/MemberVO.java
+++ BookingCatch/src/main/java/com/smhrd/model/MemberVO.java
@@ -1,8 +1,17 @@
 package com.smhrd.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class MemberVO {
-	
+	@NonNull
 	private String id;
+	@NonNull
 	private String pw;
 	private String name;
 	private String birth;
